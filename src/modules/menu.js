6f4bcb6d3aa9dd1d83e405723344fce7dacb0ec2
Rinakae <simi1993@mail.ru>
2022-03-13 13:17:41 +0300

    add scroll menu link

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,17 +1,45 @@
 const menu = () => {
-    const menuBtn = document.querySelector(".menu");
-    const menu = document.querySelector("menu");
-    const closeBtn = menu.querySelector(".close-btn");
-    const menuItems = menu.querySelectorAll("ul>li>a");
+  const menuBtn = document.querySelector(".menu");
+  const menu = document.querySelector("menu");
+  const closeBtn = menu.querySelector(".close-btn");
+  const menuItems = menu.querySelectorAll("ul>li>a");
+  const serviceBlockBtn = document.querySelector('a[href^="#service-block"');
+  
+  const handleMenu = () => {
+    menu.classList.toggle("active-menu");
+  };
 
-    const handleMenu = () => {
-        menu.classList.toggle("active-menu");
-    };
+  menuBtn.addEventListener("click", handleMenu);
+  closeBtn.addEventListener("click", handleMenu);
 
-    menuBtn.addEventListener("click", handleMenu);
-    closeBtn.addEventListener("click", handleMenu);
+  //menuItems.forEach(menuItem => menuItem.addEventListener("click", handleMenu));
 
-    menuItems.forEach(menuItem => menuItem.addEventListener("click", handleMenu));
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener("click", (event) => {
+      handleMenu();
+
+      event.preventDefault();      
+        const anchor = menuItem.getAttribute('href');
+
+        document.querySelector(anchor).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+
+    });
+
+  });
+
+  serviceBlockBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+    document.querySelector("#service-block").scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+  });
+
 };
 
 export default menu;
