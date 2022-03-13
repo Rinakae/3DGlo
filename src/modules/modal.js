f195a1cfe86fa589f85161f2b621a9763b02a3f9
Rinakae <simi1993@mail.ru>
2022-03-13 12:27:35 +0300

    animate modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,38 +1,51 @@
 const modal = () => {
+  
   const modal = document.querySelector(".popup");
   const buttons = document.querySelectorAll(".popup-btn");
   const closeBtn = modal.querySelector(".popup-close");
+  const popupContent = modal.querySelector(".popup-content");
 
   let idInterval;
-  let stop = modal.style.transform;
-  const animate = () => {
+  let count = 0;  
+
+  const animate = () => {    
+    count = count + 0.5;
     idInterval = requestAnimationFrame(animate);
-   // modal.style.display = "block";
-    if (stop) {
-      modal.style.transform = "translateX(100%)";
+
+    if (count < 10) {
+      popupContent.style.transform = "translateY("+count +"%)";
+      
     } else {
+      count = 0;
       cancelAnimationFrame(idInterval);
-    }    
+    }   
   };
 
   buttons.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-      modal.style.display = "block";
-      e.preventDefault();
-      idInterval = requestAnimationFrame(animate);
+    btn.addEventListener('click', () => {
+      modal.style.display = 'block';
+
+      let width = document.documentElement.clientWidth;            
+      if (width < 767) {
+        cancelAnimationFrame(idInterval);
+      } else {
+        animate();
+      }
+
     });
   });
+
   /*
   buttons.forEach(btn => {
     btn.addEventListener("click", () => {
       modal.style.display = "block";          
     });
   });
-*/
+  */
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";    
-  });  
-
+  }); 
+  
 };
 
 export default modal;
