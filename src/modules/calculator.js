7db96e4ddcf96555e3ee124e88c6e97aa19029a1
Rinakae <simi1993@mail.ru>
2022-03-15 19:58:11 +0300

    fix calculator valid input

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,38 +1,18 @@
 const calculator = () => {
-  const calcSquare = document.querySelector(".calc-square");
-  const calcCount = document.querySelector(".calc-count");
-  const calcDay = document.querySelector(".calc-day");
+  const calcItem = document.querySelectorAll(".calc-item:nth-child(n + 2)");  
 
-  calcSquare.addEventListener("input", (event) => {
-    event.preventDefault();
+  calcItem.forEach(item => {
+    item.addEventListener("input", (event) => {
+      event.preventDefault();
 
-    if (!event.target.value.match(/^\d+$/)) {
-      alert("Введите, пожалуйста, числовое значение");
-      calcSquare.value = "";
-      return false;
-    } 
+      if (!event.target.value.match(/^\d+$/)) {
+        alert("Введите, пожалуйста, числовое значение");
+        item.value = "";
+        return false;
+      } 
+    });
   });
 
-  calcCount.addEventListener("input", (event) => {
-    event.preventDefault();
-
-    if (!event.target.value.match(/^\d+$/)) {
-      alert("Введите, пожалуйста, числовое значение");
-      calcCount.value = "";
-      return false;
-    } 
-  });
-
-  calcDay.addEventListener("input", (event) => {
-    event.preventDefault();
-
-    if (!event.target.value.match(/^\d+$/)) {
-      alert("Введите, пожалуйста, числовое значение");
-      calcDay.value = "";
-      return false;
-    } 
-  });
-   
 };
 
 export default calculator;
