d7f4114dc4a63424a4a1847a4c4de018185f78c5
Rinakae <simi1993@mail.ru>
2022-03-16 22:20:40 +0300

    toggleMenu deleg

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from './modules/menu';
 import modal from './modules/modal';
 import calculator from './modules/calculator';
 import validForm from './modules/validForm';
+import tabs from './modules/tabs';
 
 timer("14 march 2022");
 menu();
 modal();
 calculator();
 validForm();
+tabs();
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,38 +1,37 @@
 const menu = () => {
   const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
-  const closeBtn = menu.querySelector(".close-btn");
-  const menuItems = menu.querySelectorAll("ul>li>a");
+  //const closeBtn = menu.querySelector(".close-btn");
+  //const menuItems = menu.querySelectorAll("ul>li>a");
   const serviceBlockBtn = document.querySelector('a[href^="#service-block"');
   
-  const handleMenu = () => {
+  const toggleMenu = () => {
     menu.classList.toggle("active-menu");
   };
 
-  menuBtn.addEventListener("click", handleMenu);
-  closeBtn.addEventListener("click", handleMenu);
+  menuBtn.addEventListener("click", toggleMenu);
 
-  //menuItems.forEach(menuItem => menuItem.addEventListener("click", handleMenu));
+  menu.addEventListener("click", (e) => {
 
-  menuItems.forEach(menuItem => {
-    menuItem.addEventListener("click", (event) => {
-      handleMenu();
+    if (e.target.classList.contains('close-btn')) {
+      e.preventDefault();
+      menu.classList.toggle('active-menu');
+    }
 
-      event.preventDefault();      
-        const anchor = menuItem.getAttribute('href');
-
-        document.querySelector(anchor).scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-
-    });
+    if (e.target.closest('ul>li>a')) {
+      e.preventDefault();      
+      const anchor = e.target.getAttribute('href');        
+      document.querySelector(anchor).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+      toggleMenu();
+    }
 
   });
 
   serviceBlockBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    
+    event.preventDefault();    
     document.querySelector("#service-block").scrollIntoView({
       behavior: 'smooth',
       block: 'start'
@@ -43,3 +42,23 @@ const menu = () => {
 };
 
 export default menu;
+
+/*
+  
+  closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    handleMenu();
+  } );
+
+  menuItems.forEach(menuItem => {
+    menuItem.addEventListener("click", (event) => {
+      handleMenu();
+
+      event.preventDefault();      
+        
+        });
+
+    });
+
+  });
+*/  
