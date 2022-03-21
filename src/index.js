1d7f26c7c974ea60520b56cee167af9ea56a8030
Rinakae <simi1993@mail.ru>
2022-03-21 20:27:09 +0300

    create slider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import calculator from './modules/calculator';
 import validForm from './modules/validForm';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer("14 march 2022");
 menu();
@@ -11,3 +12,4 @@ modal();
 calculator();
 validForm();
 tabs();
+slider();
